cb2df4e20f36ad4ce746b5883f10e3bd2a0e11e1
wangqs1990 <283648304@qq.com>
2014-03-03 17:11:23 +0800

    implement block and extends

--- test/test.js
+++ test/test.js
@@ -1,12 +1,14 @@
-var fs = require("fs");
-var code = fs.readFileSync("./test.html", { "encoding": "utf8" });
-var env = new (require("../lib/environment").Environment)();
-var template = env.getTemplate(code);
+var Environment = require("../lib/environment").Environment;
+var FileSystemLoader = require("../lib/loaders").FileSystemLoader;
+var env = new Environment({
+    loader: new FileSystemLoader("./")
+});
+var template = env.getTemplate("test.html");
+require('fs').writeFileSync('./out.html', template.init.toString());
 var result = template.render(
   {
     title: "I'm title",
     name: "I'm name"
   }
 );
-require('fs').writeFileSync('./out.html', template.rootFunc.toString(), {encoding: "utf8"});
 console.log(result);
